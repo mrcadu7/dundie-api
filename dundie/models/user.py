d1d08bd104551fd9003256b6077b06a1cd18eff5
Cadú <c.jrock@hotmail.com>
2023-09-18 18:22:49 -0300

    Concluindo Auth

--- dundie/models/user.py
+++ dundie/models/user.py
@@ -1,7 +1,9 @@
 """User related data models"""
 from typing import Optional
 from sqlmodel import Field, SQLModel
-from dundie.security import HashedPassword
+from dundie.security import HashedPassword, get_password_hash
+from pydantic import BaseModel, root_validator
+from fastapi import HTTPException, status
 
 
 class User(SQLModel, table=True):
@@ -26,3 +28,59 @@ class User(SQLModel, table=True):
 def generate_username(name: str) -> str:
     """Generates a slug username from a name"""
     return name.lower().replace(" ", "-")
+
+
+class UserResponse(BaseModel):
+    """Serializer for User Response"""
+
+    name: str
+    username: str
+    dept: str
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
+    currency: str
+
+
+class UserRequest(BaseModel):
+    """Serializer for User request payload"""
+
+    name: str
+    email: str
+    dept: str
+    password: str
+    currency: str = "USD"
+    username: Optional[str] = None
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
+
+    @root_validator(pre=True)
+    def generate_username_if_not_set(cls, values):
+        """Generates username if not set"""
+        if values.get("username") is None:
+            values["username"] = generate_username(values["name"])
+        return values
+
+
+class UserProfilePatchRequest(BaseModel):
+    avatar: Optional[str] = None
+    bio: Optional[str] = None
+
+
+class UserPasswordPatchRequest(BaseModel):
+    password: str
+    password_confirm: str
+
+    @root_validator(pre=True)
+    def check_passwords_match(cls, values):
+        """Checks if passwords match"""
+        if values.get("password") != values.get("password_confirm"):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Passwords do not match"
+            )
+        return values
+
+    @property
+    def hashed_password(self) -> str:
+        """Returns hashed password"""
+        return get_password_hash(self.password)
---
+++ dundie/routes/user.py
@@ -0,0 +1,107 @@
+from typing import List
+
+from fastapi import APIRouter, Body, BackgroundTasks
+from fastapi.exceptions import HTTPException
+from sqlmodel import Session, select
+from sqlalchemy.exc import IntegrityError
+
+from dundie.tasks.user import try_to_send_pwd_reset_email
+from dundie.db import ActiveSession
+from dundie.models.user import (User,
+                                UserRequest,
+                                UserResponse,
+                                UserProfilePatchRequest,
+                                UserPasswordPatchRequest)
+from dundie.auth import SuperUser, AuthenticatedUser, CanChangeUserPassword
+
+router = APIRouter()
+
+
+@router.get("/", response_model=List[UserResponse])
+async def list_users(*, session: Session = ActiveSession):
+    """List all users."""
+    users = session.exec(select(User)).all()
+    return users
+
+
+@router.get("/{username}/", response_model=UserResponse)
+async def get_user_by_username(
+    *, session: Session = ActiveSession, username: str
+):
+    """Get user by username"""
+    query = select(User).where(User.username == username)
+    user = session.exec(query).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@router.post("/", response_model=UserResponse, status_code=201, dependencies=[SuperUser])
+async def create_user(*, session: Session = ActiveSession, user: UserRequest):
+    """Creates new user"""
+    if session.exec(select(User).where(User.username == user.username)).first():
+        raise HTTPException(status_code=409, detail="Username already taken")
+
+    db_user = User.from_orm(user)  # transform UserRequest in User
+    session.add(db_user)
+    try:
+        session.commit()
+    except IntegrityError:
+        raise HTTPException(status_code=500, detail="Database IntegrityError")
+
+    session.refresh(db_user)
+    return db_user
+
+
+@router.patch("/{username}/", response_model=UserResponse)
+async def update_user(
+    *,
+    session: Session = ActiveSession,
+    patch_data: UserProfilePatchRequest,
+    current_user: User = AuthenticatedUser,
+    username: str
+):
+    user = session.exec(select(User).where(User.username == username)).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if user.id != current_user.id and not current_user.superuser:
+        raise HTTPException(status_code=403, detail="You can only update your own profile")
+
+    # Update
+    if patch_data.avatar is not None:
+        user.avatar = patch_data.avatar
+
+    if patch_data.bio is not None:
+        user.bio = patch_data.bio
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+
+@router.post("/{username}/password/", response_model=UserResponse)
+async def change_password(
+    *,
+    session: Session = ActiveSession,
+    patch_data: UserPasswordPatchRequest,
+    user: User = CanChangeUserPassword
+):
+    user.password = patch_data.hashed_password  # pyright: ignore
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+
+@router.post("/pwd_reset_token/")
+async def send_password_reset_token(
+    *,
+    email: str = Body(embed=True),
+    background_tasks: BackgroundTasks,
+):
+    """Sends an email with the token to reset password."""
+    background_tasks.add_task(try_to_send_pwd_reset_email, email=email)
+    return {
+        "message": "If we found a user with that email, we sent a password reset token to it."
+    }
